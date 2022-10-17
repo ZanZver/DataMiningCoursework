081528cc078b48d9ae13d1f2005bd9614072b405
ZanZver <zan.zver98@gmail.com>
2022-10-17 17:40:25 +0100

    Merged dataLoaderOriginal and dataLoaderLite
    Now, there is a single function with the name of dataLoader that does the job for both dataLoaderOriginal and dataLoaderLite.
    By default, lite dataset is used, change the if statement to false if you want to use original dataset.

--- Code/Code.R
+++ Code/Code.R
@@ -26,10 +26,6 @@
 #==============================================================================================================
 # Data import
 #==============================================================================================================
-# Lite dataset, used for dev (only 10k rows)
-dataPathLite <- "hotel_bookings_lite.csv"
-# Original dataset, used for test & prod
-dataPathOriginal <- "hotel_bookings.csv"
 
 # Our dataset paths
 # Daniels data path
@@ -39,46 +35,21 @@ dataFolderMihai <- "~/Documents/BCU2/Masters/" # MIHAI change that to whatever y
 # Zans data path
 dataFolderZan <- "~/Documents/BCU2/Masters/CMP7206-A-S1-2022:3_Data_Mining/Coursework/DataMiningCoursework/Data/"
 
-# Function to load all of the data
-dataLoaderOriginal <- function(dataFolder, dataPathOriginal) {
-  tryCatch(               
-    expr = {
-      # Join persons data path with original data path
-      dataOriginal <- paste(dataFolder, dataPathOriginal, sep = "")
-      # Read CSV from dataOriginal path
-      hotel_bookings <- read.csv(dataOriginal, stringsAsFactors=TRUE)
-      # Return CSV
-      return(hotel_bookings)
-    },
-    error = function(e){
-      # Inform the user on whose profile the error occurred
-      print(sprintf("Error for user: %s", user))
-      # Print out the error
-      print(e)
-      # Return nothing
-      return(NULL)
-    },
-    warning = function(w){
-      # Inform the user on whose profile the warning occurred
-      print(sprintf("Warning for user: %s", user))
-      # Print out the warning
-      print(w)
-      # Return nothing
-      return(NULL)
-    },
-    finally = {
-      # Let the user know once the function has finished with execution
-      print(sprintf("Dataloader for %s finished.", user))
-    }
-  )
+# Decide which file to use, lite dataset is used by default
+if(TRUE){
+  # Lite dataset, used for dev (only 10k rows)
+  fileName <- "hotel_bookings_lite.csv"
+}else{
+  # Original dataset, used for test & prod
+  fileName <- "hotel_bookings.csv"
 }
 
 # Function to load all of the data
-dataLoaderLite <- function(dataFolder, dataPathLite, user) {
+dataLoader <- function(dataFolder, fileName, user) {
   tryCatch(               
     expr = {
       # Join persons data path with lite data path
-      dataLite <- paste(dataFolder, dataPathLite, sep = "")
+      dataLite <- paste(dataFolder, fileName, sep = "")
       # Read CSV from dataLite path
       hotel_bookings <- read.csv(dataLite, stringsAsFactors=TRUE)
       # Return CSV
@@ -113,20 +84,21 @@ while(userNum < 5){
   print("=============================================================")
   # Go across the user profiles, save the data or throw an error if no data is saved at the end
   switch( userNum,  
-          hotel_bookings <- dataLoaderLite(dataFolderDaniel, dataPathLite, "Daniel"),  
-          hotel_bookings <- dataLoaderLite(dataFolderMihai, dataPathLite, "Mihai"),  
-          hotel_bookings <- dataLoaderLite(dataFolderZan, dataPathLite, "Zan"),  
+          hotel_bookings <- dataLoader(dataFolderDaniel, fileName, "Daniel"),  
+          hotel_bookings <- dataLoader(dataFolderMihai, fileName, "Mihai"),  
+          hotel_bookings <- dataLoader(dataFolderZan, fileName, "Zan"),  
           stop("No data has been loaded") # Stops the code HERE if no data has been loaded
   ) 
   userNum <- userNum + 1 
   
   # Once the data is found (from the user), break the loop
   if(!is.null(hotel_bookings)){
+    # Prints first 6 rows so you can see the data
+    print(head(hotel_bookings))
     break
   }
 }
 print("=============================================================")
-print("Test")
 
 
 #==============================================================================================================
